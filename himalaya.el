09f89f424b205dfa051adc171a1842d8106c3ff5
dantecatalfamo <dante.catalfamo@gmail.com>
2021-10-26 19:03:25 -0400

    Improve how himalaya is called

--- himalaya.el
+++ himalaya.el
@@ -36,51 +36,70 @@
   "Name or location of the himalaya executable."
   :type 'text)
 
+(defun himalaya--run (&rest args)
+  "Run himalaya with ARGS.
+Results are returned as a string. Signals a lisp error and
+displaus the output on non-zero exit."
+  (with-temp-buffer
+    (let* ((args (flatten-list args))
+           (ret (apply #'call-process himalaya-executable nil t nil args))
+           (output (buffer-string)))
+      (unless (eq ret 0)
+        (with-current-buffer-window "*himalaya error*" nil nil
+          (insert output))
+        (error "Himalaya exited with a non-zero status"))
+      output)))
+
 (defun himalaya--run-json (&rest args)
   "Run himalaya with ARGS arguments.
 The result is parsed as JSON and returned."
-  (let* ((args (string-join args " "))
-         (cmd (concat himalaya-executable " -o json " args))
-         (output (shell-command-to-string cmd)))
-    (when (string-prefix-p "Error:" output)
-      (error "%s" (car (split-string output "\n"))))
+  (let ((args (append '("-o" "json") args)))
     ;; Remove { "response": [...] } wrapper
-    (cadr (json-parse-string output :object-type 'plist :array-type 'list))))
+    (cadr (json-parse-string (himalaya--run args)
+                             :object-type 'plist
+                             :array-type 'list))))
 
 (defun himalaya--mailbox-list (&optional account)
   "Return a list of mailboxes for ACCOUNT.
 If ACCOUNT is nil, the default account is used."
-  (himalaya--run-json (when account (concat "-a " account)) "mailboxes"))
+  (himalaya--run-json (when account (list "-a" account)) "mailboxes"))
 
 (defun himalaya--message-list (&optional account mailbox page page-size)
   "Return a list of emails from ACCOUNT in MAILBOX.
 Paginate using PAGE of PAGE-SIZE.
 If ACCOUNT, MAILBOX, PAGE, or PAGE-SIZE are nil, the default values are used."
-  (himalaya--run-json (when account (concat "-a " account))
-                      (when mailbox (concat "-m " mailbox))
+  (himalaya--run-json (when account (list "-a" account))
+                      (when mailbox (list "-m" mailbox))
                       "list"
-                      (when page (concat "-p " page))
-                      (when page-size "-s " page-size)))
+                      (when page (list "-p" (format "%s" page)))
+                      (when page-size (list "-s" (format "%s" page-size)))))
 
 (defun himalaya--message-read (uid &optional account raw html)
-  "Return the contents of message with UID (string) from ACCOUNT.
+  "Return the contents of message with UID from ACCOUNT.
 If ACCOUNT is nil, use the default. If RAW is non-nil, return the
 raw contents of the email including headers. If HTML is non-nil,
 return the HTML version of the email, otherwise return the plain
 text version."
-
   (himalaya--run-json (when account (concat "-a " account))
                       "read"
-                      uid
+                      (format "%s" uid) ; Ensure uid is a string
                       (when raw "-r")
-                      (when html "-t html")))
+                      (when html (list "-t" "html"))))
 
 (defun himalaya--message-copy (uid target &optional account)
-  "Move message wit UID (string) to TARGET mailbox on ACCOUNT.
+  "Copy message with UID to TARGET mailbox on ACCOUNT.
 If ACCOUNT is nil, use the default."
-  (himalaya--run-json (when account (concat "-a " account))
+  (himalaya--run-json (when account (list "-a" account))
                       "copy"
-                      uid
+                      (format "%s" uid)
+                      target))
+
+(defun himalaya--message-move (uid target &optional account)
+  "Move message with UID to TARGET mailbox on ACCOUNT.
+If ACCOUNT is nil, use the default."
+  (himalaya--run-json (when account (list "-a" account))
+                      "move"
+                      (format "%s" uid)
                       target))
 
 (provide 'himalaya)
